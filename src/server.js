f1001ebf2fa0fc66965321c0b89e4eabdf8be77c
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2016-02-27 13:04:13 +0200

    stuff

--- src/server.js
+++ src/server.js
@@ -1,18 +1,39 @@
-import Express from 'express';
-import HTTP from 'http';
-import SocketIO from 'socket.io';
-import SocketIOP2P from 'socket.io-p2p-server';
+import HTTP from 'http'
+import SocketIO from 'socket.io'
+import SocketIOP2P from 'socket.io-p2p-server'
+import Redis from 'redis'
+import Crypto from 'crypto'
 
-var app = Express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var redisClient = Redis.createClient()
+redisClient.on('error', err => console.error(`Redis error: ${err}`))
 
-var port = process.env.P2PT_PORT ? process.envt.P2PT_PORT : 3000;
-var server = HTTP.Server(app);
-var io = SocketIO(server);
-io.use(SocketIOP2P.Server);
+var server = HTTP.createServer()
 
+var port = process.env.P2PT_PORT ? process.envt.P2PT_PORT : 3000
 server.listen(port, function () {
-  console.log('P2PT listening on port ' + port);
+  console.log('P2PT listening on port ' + port)
 });
+
+var io = SocketIO(server)
+io.on('connection', socket => {
+    socket.on('ask-token', () => {
+        Crypto.randomBytes(16, (err, buf) => {
+            if (err) {
+                console.error(err)
+                socket.emit('error', 'failed to create token')
+                return
+            }
+
+            var token = buf.toString('hex')
+            socket.join(token)
+            SocketIOP2P.Server(socket, null, token)
+            socket.emit('set-token-ok', token)
+        })
+    })
+
+    socket.on('set-token', token => {
+        socket.join(token)
+        SocketIOP2P.Server(socket, null, token)
+        socket.emit('set-token-ok', token)
+    })
+})
