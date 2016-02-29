621e403bf49d2773a7cf9a3f5ea9ef60582ea0dd
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2016-02-29 03:24:11 +0200

    stricter rate-limits

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ function doRateLimit(socket, next) {
         socket.handshake.headers['x-forwarded-for']
 
     redisClient.get('addr_' + address, (err, val) => {
-        if(val && val > 100) {
+        if(val && val > 32) {
             socket.disconnect()
             return
         }
